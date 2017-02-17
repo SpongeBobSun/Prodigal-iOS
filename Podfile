e20e91fdac71b82c1b87b80d71997e8b5631c208
bob.sun <widekuan@outlook.com>
2017-02-17 15:57:32 +0800

    Init project

---
+++ Podfile
@@ -0,0 +1,12 @@
+project 'Prodigal/Prodigal.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Prodigal' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Prodigal
+
+end
---
+++ Prodigal/Prodigal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prodigal
+//
+//  Created by bob.sun on 17/02/2017.
+//  Copyright © 2017 bob.sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
