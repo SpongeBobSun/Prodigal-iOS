cd31fb4b26e08ffcd79ef01e5d997dbeaa96c2d1
bob.sun <widekuan@outlook.com>
2017-03-16 15:26:30 +0800

    update pubs

--- Prodigal/Prodigal/Utils/PubSub.swift
+++ Prodigal/Prodigal/Utils/PubSub.swift
@@ -8,10 +8,21 @@
 
 import Foundation
 
+fileprivate class PubSubObserver {
+    var name: String! = nil
+    var observer: Any! = nil
+    
+    convenience init(name: String, obs: Any) {
+        self.init()
+        self.name = name
+        self.observer = obs
+    }
+}
+
 class PubSub {
     static let queue = DispatchQueue(label: "sun.bob.bender.pubsubQ")
     static let instance = PubSub()
-    private var map:[UInt: [Any]] = [:]
+    private var map:[UInt: [PubSubObserver]] = [:]
     
     class func publish(name:String, sender: Any) {
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: name), object: nil)
@@ -35,24 +46,50 @@ class PubSub {
     
     class func subscribe(target: AnyObject, name: String, handler: @escaping((Notification) -> Void)) {
         let key = UInt(bitPattern: ObjectIdentifier(target))
+        
+        if instance.map[key] != nil {
+            for obs in instance.map[key]! {
+                if obs.name == name {
+                    return
+                }
+            }
+        }
         let observer = NotificationCenter.default.addObserver(forName: NSNotification.Name(name), object: nil, queue: OperationQueue(), using: handler)
         queue.sync {
             if let observers = instance.map[key]  {
-                instance.map[key] = observers + [observer]
+                instance.map[key] = observers + [PubSubObserver(name: name, obs: observer)]
             } else {
-                instance.map[key] = [observer]
+                instance.map[key] = [PubSubObserver(name: name, obs: observer)]
             }
         }
     }
     
     class func subOnMainThread(target: AnyObject, name: String, handler: @escaping((Notification) -> Void)) {
         let key = UInt(bitPattern: ObjectIdentifier(target))
+        if instance.map[key] != nil {
+            for obs in instance.map[key]! {
+                if obs.name == name {
+                    return
+                }
+            }
+        }
         let observer = NotificationCenter.default.addObserver(forName: Notification.Name(name), object: nil, queue: OperationQueue.main, using: handler)
         queue.sync {
             if let observers = instance.map[key]  {
-                instance.map[key] = observers + [observer]
+                instance.map[key] = observers + [PubSubObserver(name: name, obs: observer)]
             } else {
-                instance.map[key] = [observer]
+                instance.map[key] = [PubSubObserver(name: name, obs: observer)]
+            }
+        }
+    }
+    
+    class func unsubscribe(target: AnyObject, name: String) {
+        let key = UInt(bitPattern: ObjectIdentifier(target))
+        if let psbObs = instance.map[key] {
+            for obs in psbObs {
+                if obs.name == name {
+                    NotificationCenter.default.removeObserver(obs)
+                }
             }
         }
     }
