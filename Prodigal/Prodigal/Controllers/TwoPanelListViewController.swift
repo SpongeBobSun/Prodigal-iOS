5475efafe3a319f843cce3db1ad9297b9d63bf09
bob.sun <widekuan@outlook.com>
2017-03-29 10:58:01 +0800

    Add empty view for now playing widget

--- Prodigal/Prodigal/Controllers/TwoPanelListViewController.swift
+++ Prodigal/Prodigal/Controllers/TwoPanelListViewController.swift
@@ -2,9 +2,42 @@
 //  TwoPanelListViewController.swift
 //  Prodigal
 //
-//  Created by bob.sun on 23/02/2017.
-//  Copyright © 2017 bob.sun. All rights reserved.
+//   Copyright 2017 Bob Sun
 //
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+//  Created by bob.sun on 28/03/2017.
+//
+//          _
+//         ( )
+//          H
+//          H
+//         _H_
+//      .-'-.-'-.
+//     /         \
+//    |           |
+//    |   .-------'._
+//    |  / /  '.' '. \
+//    |  \ \ @   @ / /
+//    |   '---------'
+//    |    _______|
+//    |  .'-+-+-+|              I'm going to build my own APP with blackjack and hookers!
+//    |  '.-+-+-+|
+//    |    """""" |
+//    '-.__   __.-'
+//         """
+//
+
 
 import UIKit
 import Koloda
@@ -382,7 +415,9 @@ class NowPlayingWidget: UIView {
     
     func config(media: MPMediaItem?) {
         if media == nil {
-            //TODO
+            imageView.image = #imageLiteral(resourceName: "ic_album")
+            title.text = NSLocalizedString("Nothing", comment: "")
+            artist.text = NSLocalizedString("Nobody", comment: "")
             return
         }
         imageView.image = media!.artwork?.image(at: CGSize(width: 200, height: 200)) ?? #imageLiteral(resourceName: "ic_album")
