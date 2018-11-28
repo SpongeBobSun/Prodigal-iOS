85852ee8b9fb2135d57b422b3eed8cf4cd7ac291
SunKuan <sunkuan@meituan.com>
2018-11-28 11:22:08 +0800

    Fix UITableView issue caused by iOS 11 update (Seriously???)
    When programmatically reloading & scrolling at the same time, the scroll animation will be interrupted by tableview reload. This issue is introduced in iOS 11 and still exists in current iOS version (12). Reloading & user interaction scrolling will not trigger this issue. Solution is adding a 0 second animation block to reloading and scrolling the tableview in completion block.

---
+++ Prodigal/Prodigal/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Prodigal
+//
+//  Created by Bob on 2018/11/27.
+//  Copyright © 2018 bob.sun. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+let LIST_ITEM_VIEW_HEIGHT: CGFloat = 50
